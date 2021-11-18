99ff4387f87d3539303b9e4cd21c672baef229d2
Lily Xie <lxie9195@gmail.com>
2021-11-18 18:11:41 -0500

    update shiny app to render map
    update shiny app to render map

--- MAUP_App/app.R
+++ MAUP_App/app.R
@@ -9,40 +9,49 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# read in interactive map RDS
+blocks <- readRDS("all_blocks.RDS")
+
+# Define UI for application 
 ui <- fluidPage(
+  mainPanel(
+    leafletOutput("blocks")
+  )
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    # # Application title
+    # titlePanel("Old Faithful Geyser Data"),
+    # 
+    # # Sidebar with a slider input for number of bins 
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         sliderInput("bins",
+    #                     "Number of bins:",
+    #                     min = 1,
+    #                     max = 50,
+    #                     value = 30)
+    #     ),
+    # 
+    #     # Show a plot of the generated distribution
+    #     mainPanel(
+    #        plotOutput("distPlot")
+    #     )
+    # )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a map
 server <- function(input, output) {
+  
+  output$blocks <- renderLeaflet(map2)
+  
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # })
 }
 
 # Run the application 
