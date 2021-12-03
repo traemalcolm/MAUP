2bbd1682d8166066fc45814dd6bde987711d0ce6
Taelor Malcolm <tmalcolm@gsd.harvard.edu>
2021-12-02 21:25:40 -0500

    Ducky and maps in tabs

--- MAUP_App/app.R
+++ MAUP_App/app.R
@@ -17,7 +17,6 @@ library(leaflet.extras)
 library(shiny)
 library(googlesheets4)
 
-#read in shapefile 
 censusblocks <- st_read("censusblocks/censusblocks_fire_Clip.shp")
 
 ### MAKE INTERACTIVE MAP
@@ -32,7 +31,7 @@ pal <- colorBin(palette = "OrRd", bins = bin, domain = censusblocks$COUNT)
 
 # create icon marker
 fireIcon <- makeIcon(
-  iconUrl = 'https://images.vexels.com/media/users/3/149795/isolated/lists/59a3259ace3f62753d684cb15f66d989-firefighter-hat-icon.png',
+  iconUrl = 'firehat.png',
   iconWidth = 10,
   iconHeight = 10,
   iconAnchorX = 100,
@@ -72,43 +71,329 @@ blocks_interactive <- censusblocks %>%
             title = "fire incidents", 
             opacity = 0.7)
 
+censustracts <- st_read("censustracts/censustracts_fire_Clip.shp")
+
+### MAKE INTERACTIVE MAP
+labels <-sprintf(
+  "<strong>%s</strong><br/>%s fire incidents in September 2021",
+  censustracts$NAME20, censustracts$COUNT) %>%
+  lapply(htmltools::HTML)
+
+#color palette 
+bin <-  c(0, 12, 23, 39, 72, 149)
+pal <- colorBin(palette = "OrRd", bins = bin , domain = censustracts$COUNT)
+
+tracts_interactive <- censustracts %>%
+  st_transform(crs = st_crs("+init=epsg:4326")) %>%
+  leaflet() %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>% 
+  addPolygons(label = labels, 
+              stroke = FALSE, 
+              smoothFactor = .5, 
+              opacity = 1, 
+              fillOpacity = 0.7, 
+              fillColor = ~ pal(COUNT), 
+              highlightOptions = highlightOptions(weight = 5,
+                                                  fillOpacity = 1, 
+                                                  color = "white", 
+                                                  opacity = 1, 
+                                                  bringToFront = TRUE)) %>%
+  addDrawToolbar(
+    targetGroup = "draw",
+    polylineOptions = FALSE,
+    polygonOptions = FALSE,
+    circleOptions = FALSE,
+    rectangleOptions = FALSE,
+    circleMarkerOptions = FALSE,
+    markerOptions = drawMarkerOptions(markerIcon = fireIcon),
+    editOptions = editToolbarOptions(
+      selectedPathOptions = selectedPathOptions()
+    )
+  ) %>% 
+  
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ COUNT, 
+            title = "fire incidents", 
+            opacity = 0.7)
+
+neighborhoods <- st_read("neighborhoods/neighborhoods_fire_Clip.shp")
+
+
+### MAKE INTERACTIVE MAP
+labels <-sprintf(
+  "<strong>%s</strong><br/>%s fire incidents in September 2021",
+  neighborhoods$Name, neighborhoods$COUNT) %>%
+  lapply(htmltools::HTML)
+
+#color palette 
+bin <- c(0, 48, 148, 264, 460, 739)
+pal <- colorBin(palette = "OrRd", bins = bin, domain = neighborhoods$COUNT)
+
+neighborhoods_interactive <- neighborhoods %>%
+  st_transform(crs = st_crs("+init=epsg:4326")) %>%
+  leaflet() %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>% 
+  addPolygons(label = labels, 
+              stroke = FALSE, 
+              smoothFactor = .5, 
+              opacity = 1, 
+              fillOpacity = 0.7, 
+              fillColor = ~ pal(COUNT), 
+              highlightOptions = highlightOptions(weight = 5,
+                                                  fillOpacity = 1, 
+                                                  color = "white", 
+                                                  opacity = 1, 
+                                                  bringToFront = TRUE)) %>%
+  addDrawToolbar(
+    targetGroup = "draw",
+    polylineOptions = FALSE,
+    polygonOptions = FALSE,
+    circleOptions = FALSE,
+    rectangleOptions = FALSE,
+    circleMarkerOptions = FALSE,
+    markerOptions = drawMarkerOptions(markerIcon = fireIcon),
+    editOptions = editToolbarOptions(
+      selectedPathOptions = selectedPathOptions()
+    )
+  ) %>% 
+  
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ COUNT, 
+            title = "fire incidents", 
+            opacity = 0.7)
+
+
+#read in shapefile 
+wards <- st_read("wards/wards_fire_Clip.shp")
+
+### MAKE INTERACTIVE MAP
+labels <-sprintf(
+  "<strong>%s</strong><br/>%s fire incidents in September 2021",
+  wards$WARD, wards$COUNT) %>%
+  lapply(htmltools::HTML)
+
+#color palette 
+bin <- c(0, 124, 176, 231, 348, 534)
+pal <- colorBin(palette = "OrRd", bins = bin, domain = wards$COUNT)
+
+wards_interactive <- wards %>%
+  st_transform(crs = st_crs("+init=epsg:4326")) %>%
+  leaflet() %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>% 
+  addPolygons(label = labels, 
+              stroke = FALSE, 
+              smoothFactor = .5, 
+              opacity = 1, 
+              fillOpacity = 0.7, 
+              fillColor = ~ pal(COUNT), 
+              highlightOptions = highlightOptions(weight = 5,
+                                                  fillOpacity = 1, 
+                                                  color = "white", 
+                                                  opacity = 1, 
+                                                  bringToFront = TRUE)) %>%
+  addDrawToolbar(
+    targetGroup = "draw",
+    polylineOptions = FALSE,
+    polygonOptions = FALSE,
+    circleOptions = FALSE,
+    rectangleOptions = FALSE,
+    circleMarkerOptions = FALSE,
+    markerOptions = drawMarkerOptions(markerIcon = fireIcon),
+    editOptions = editToolbarOptions(
+      selectedPathOptions = selectedPathOptions()
+    )
+  ) %>% 
+  
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ COUNT, 
+            title = "wards fire incidents", 
+            opacity = 0.7)
+
+zipcodes <- st_read("zipcodes/zipcodes_fire_Clip.shp")
+
+### MAKE INTERACTIVE MAP
+labels <-sprintf(
+  "<strong>%s</strong><br/>%s fire incidents in September 2021",
+  zipcodes$ZIP5, zipcodes$COUNT) %>%
+  lapply(htmltools::HTML)
+
+#color palette 
+bin <- c(0, 21, 95, 156, 242, 402)
+pal <- colorBin(palette = "OrRd", bins = bin, domain = zipcodes$COUNT)
+
+
+zip_interactive <- zipcodes %>%
+  st_transform(crs = st_crs("+init=epsg:4326")) %>%
+  leaflet() %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>% 
+  addPolygons(label = labels, 
+              stroke = FALSE, 
+              smoothFactor = .5, 
+              opacity = 1, 
+              fillOpacity = 0.7, 
+              fillColor = ~ pal(COUNT), 
+              highlightOptions = highlightOptions(weight = 5,
+                                                  fillOpacity = 1, 
+                                                  color = "white", 
+                                                  opacity = 1, 
+                                                  bringToFront = TRUE)) %>%
+  addDrawToolbar(
+    targetGroup = "draw",
+    polylineOptions = FALSE,
+    polygonOptions = FALSE,
+    circleOptions = FALSE,
+    rectangleOptions = FALSE,
+    circleMarkerOptions = FALSE,
+    markerOptions = drawMarkerOptions(markerIcon = fireIcon),
+    editOptions = editToolbarOptions(
+      selectedPathOptions = selectedPathOptions()
+    )
+  ) %>% 
+  
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ COUNT, 
+            title = "fire incidents", 
+            opacity = 0.7)
+
+
+#read in shapefile 
+blank_grid <- st_read("blank_grid/boston_fishnet_500ft_clipped2.shp")
+grid <- st_read("grid_fire/grid_fire.shp")
+
+### MAKE INTERACTIVE MAP
+labels <-sprintf(
+  "<strong>%s</strong><br/>%s fire incidents in September 2021",
+  grid$OBJECTID, grid$COUNT) %>%
+  lapply(htmltools::HTML)
+
+#color palette
+bin <- c(0, 2, 4, 8, 14, 23)
+pal <- colorBin(palette = "OrRd", bins = bin, domain = grid$COUNT)
+
+grid_interactive <- grid %>%
+  st_transform(crs = st_crs("+init=epsg:4326")) %>%
+  leaflet() %>%
+  addProviderTiles(provider = "CartoDB.Positron") %>% 
+  addPolygons(label = labels, 
+              stroke = FALSE, 
+              smoothFactor = .5, 
+              opacity = 1, 
+              fillOpacity = 0.7, 
+              fillColor = ~ pal(COUNT), 
+              highlightOptions = highlightOptions(weight = 5,
+                                                  fillOpacity = 1, 
+                                                  color = "white", 
+                                                  opacity = 1, 
+                                                  bringToFront = TRUE)) %>%
+  addDrawToolbar(
+    targetGroup = "draw",
+    polylineOptions = FALSE,
+    polygonOptions = FALSE,
+    circleOptions = FALSE,
+    rectangleOptions = FALSE,
+    circleMarkerOptions = FALSE,
+    markerOptions = drawMarkerOptions(markerIcon = fireIcon),
+    editOptions = editToolbarOptions(
+      selectedPathOptions = selectedPathOptions()
+    )
+  ) %>% 
+  
+  addLegend("bottomright", 
+            pal = pal, 
+            values = ~ COUNT, 
+            title = "fire incidents", 
+            opacity = 0.7)
+
 # Define UI for application 
 ui <- fluidPage(
+  # App Title
+  titlePanel("Assisting Boston Fire Chief Ducky"),
+  # Sidebar with game intro 
+  sidebarPanel(
+    position = "right",
+    img(src="ducky.png",width="45%"),
+    h5("You’re our new mapping specialist, right? I’m so glad you’re here! 
+        I’m Ducky, the chief of the Boston Fire Department. I need your help to 
+        see how many fire-related incidents we had in each area last year and then we can use 
+        that information to decide how to spend our budget to better serve the Boston area next year!"),
+    h2("Game Rules"), 
+    h5("I was given these six maps to work with...what do you think? 
+       They show fire-related calls across the City, divided up by census tracts, census blocks, 
+       zip code, neighborhood, wards, and a 5x5 km grid. Place firehouses around Boston to cover the 
+       most calls by dragging and dropping firehouses using the 'Draw marker' button."),
+    br(),
+    h5("Pick one of the maps, and let's see how many fire-related calls we can cover!")
+    ),
   mainPanel(
-    leafletOutput("blocks", width = "100%", height = 800)
+    
+    
+    #img(src = 'ducky.png', align="left"),
+    # Random map button 
+    tabsetPanel(id='my_tabsetPanel',
+                tabPanel('Census Blocks',
+                         leafletOutput('blocks', width = "100%", height = 800)   
+                ), 
+                tabPanel('Census Tracts',
+                         leafletOutput('tracts', width = "100%", height = 800)   
+                ), 
+                tabPanel('Neighborhoods',
+                         leafletOutput('neighborhoods', width = "100%", height = 800)   
+                ), 
+                tabPanel('Wards', 
+                         leafletOutput('wards', width = "100%", height = 800)   
+                ),
+                tabPanel('Zip Codes', 
+                         leafletOutput('zipcodes', width = "100%", height = 800)   
+                ),
+                tabPanel('500 ftx500 ft grid', 
+                         leafletOutput('grid', width = "100%", height = 800)   
+                )
+                
+    )
   )
-
-    # # Application title
-    # titlePanel("Old Faithful Geyser Data"),
-    # 
-    # # Sidebar with a slider input for number of bins 
-    # sidebarLayout(
-    #     sidebarPanel(
-    #         sliderInput("bins",
-    #                     "Number of bins:",
-    #                     min = 1,
-    #                     max = 50,
-    #                     value = 30)
-    #     ),
-    # 
-    #     # Show a plot of the generated distribution
-    #     mainPanel(
-    #        plotOutput("distPlot")
-    #     )
-    # )
+  
 )
+
 
 # Define server logic required to draw a map
 server <- function(input, output) {
   
   # render map
-  output$blocks <- renderLeaflet(blocks_interactive)
+  
+  output$blocks <- renderLeaflet({blocks_interactive})
+  output$tracts <- renderLeaflet({tracts_interactive})
+  output$neighborhoods <- renderLeaflet({neighborhoods_interactive})
+  output$wards <- renderLeaflet({wards_interactive})
+  output$zipcodes <- renderLeaflet({zip_interactive})
+  output$grid <- renderLeaflet({grid_interactive})
+  
+  observe({
+    input$my_tabsetPanel
+    
+    tab1 <- leafletProxy('blocks') 
+    tab2 <- leafletProxy('tracts')
+    tab3 <- leafletProxy('neighborhoods')
+    tab4 <- leafletProxy('wards')  
+    tab5 <- leafletProxy('zipcodes')
+    tab6 <- leafletProxy('grid')
+      
+    
+  })
+  
   
   # load google sheet
   # TODO: update to create a new google sheet each time in a directory
   # TODO: load API creds automatically
   gsOut <- gs4_get('https://docs.google.com/spreadsheets/d/11DX7BY-xk_CvkJ7HG3MlOpQYkGCf1B3lBIQzxzJ1jUM/edit?usp=sharing')
   
+  randomVals <- eventReactive(input$nex_map, {
+    
+  })
+
   # observe marker events 
   observeEvent(input$blocks_draw_edited_features,{
     feature <- input$blocks_draw_edited_features
