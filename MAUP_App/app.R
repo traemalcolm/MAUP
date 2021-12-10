96fbdcd9e362a6a1d0d3b57d920272651a8e1c54
Taelor Malcolm <tmalcolm@gsd.harvard.edu>
2021-12-10 13:02:10 -0500

    draggable game rules

--- MAUP_App/app.R
+++ MAUP_App/app.R
@@ -146,7 +146,7 @@ ui <- fillPage(
   tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
   useShinyalert(),
   absolutePanel(class = "panel panel-default", fixed = TRUE,
-                 draggable = FALSE, top = 60, right = 20, bottom = "auto",
+                 draggable = TRUE, top = 60, right = 20, bottom = "auto",
                  width = 330, height = 590,
     style = "z-index: 5",
     fluidRow(
@@ -248,7 +248,7 @@ server <- function(input, output) {
     # marker counter 
     rv$n <- isolate(rv$n) + 1
     print(rv$n)
-    if(rv$n > 3){ 
+    if(rv$n >= 3){ 
       shinyalert("Oops!", "You've run out of fire stations.", type = "error")
       }
     
